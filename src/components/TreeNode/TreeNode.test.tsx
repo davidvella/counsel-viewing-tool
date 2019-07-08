7121e0742d1024b9336d3fb882487cc2512bce40
David Vella <david@vella.uk.com>
2019-07-08 19:43:45 +0100

    Add unit tests for tree view/node

---
+++ src/components/TreeNode/TreeNode.test.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  render, cleanup
+} from "@testing-library/react";
+// this adds custom jest matchers from jest-dom
+import "jest-dom/extend-expect";
+import TreeNode from './TreeNode';
+import TreeView from '../TreeView/TreeView';
+
+describe('<TreeNode />', () => {
+  const defaultProps = {
+    nodeId: 'node1',
+  };
+
+  afterEach(cleanup);
+
+  describe('Accessibility', () => {
+    it('should have the role `treeitem`', () => {
+      const dom = render(<TreeView>
+        <TreeNode nodeId="test" label="test" />
+      </TreeView>);
+
+      expect(dom.getAllByRole('treeitem').length > 0).toEqual(true);
+    });
+
+    it('should add the role `group` to a `ul` component if it has children', () => {
+      const { container } = render(<TreeView>
+        <TreeNode nodeId="test" label="test">
+          <TreeNode nodeId="test2" label="test" />
+        </TreeNode>
+      </TreeView>);
+
+      expect(container.querySelectorAll('ul[role="group"]').length > 0).toEqual(true);
+    });
+
+    it('should have the attribute `aria-expanded=false` if collapsed', () => {
+      const { container } = render(<TreeView>
+        <TreeNode nodeId="test" label="test">
+          <TreeNode nodeId="test2" label="test" />
+        </TreeNode>
+      </TreeView>);
+
+      expect(container.querySelectorAll('[aria-expanded=false]').length > 0).toEqual(true);
+    });
+
+    it('should have the attribute `aria-expanded=true` if expanded', () => {
+      const { container } = render(<TreeView expanded={['test']}>
+        <TreeNode nodeId="test" label="test">
+          <TreeNode nodeId="test2" label="test" />
+        </TreeNode>
+      </TreeView>);
+      expect(container.querySelectorAll('[aria-expanded=true]').length > 0).toEqual(true);
+    });
+
+    it('should not have the attribute `aria-expanded` if no children are present', () => {
+
+      const { container } = render(<TreeView>
+        <TreeNode nodeId="test" label="test" />
+      </TreeView>);
+
+      expect(container.querySelectorAll('[aria-expanded]').length > 0).toEqual(false);
+    });
+  });
+});
---
+++ src/components/TreeView/TreeView.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  render,
+  cleanup,
+  fireEvent,
+  waitForElement
+} from "@testing-library/react";
+// this adds custom jest matchers from jest-dom
+import "jest-dom/extend-expect";
+import TreeNode from '../TreeNode/TreeNode';
+import TreeView from './TreeView';
+
+describe('<TreeView />', () => {
+
+  afterEach(cleanup);
+
+  describe('onNodeToggle', () => {
+    it('should be called when a parent node is clicked', () => {
+      const handleNodeToggle = jest.fn();
+
+      const { container } = render(<TreeView onNodeToggle={handleNodeToggle}>
+        <TreeNode nodeId="1">
+          <TreeNode nodeId="2" />
+        </TreeNode>
+      </TreeView>);
+
+      fireEvent.click(container.querySelectorAll('[role="treeitem"] > *')[0]);
+
+      expect(handleNodeToggle).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Accessibility', () => {
+    it('(TreeView) should have the role `tree`', () => {
+      const { container } = render(<TreeView />);
+      expect(container.querySelectorAll('[role="tree"]').length > 0).toEqual(true);
+    });
+  });
+});
